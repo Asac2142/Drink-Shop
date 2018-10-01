c8216be19bab81b9252d478a8ace98a817817949
Andrés Alban <bethesdas101@gmail.com>
2018-09-30 21:50:36 -0500

    Drink shop

---
+++ src/formosexercise/Drink.java
@@ -0,0 +1,14 @@
+package formosexercise;
+
+public class Drink {
+    
+    private Ingredient[] drink;
+    
+    public Drink(){
+        drink = new Ingredient[4];
+    }
+    
+    public Ingredient[] getDrink(){
+        return drink;
+    }    
+}
